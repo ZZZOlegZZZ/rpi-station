7bfa5dd737f0985512afc2288f1bcc2d5b938101
Евгений Буриков <evgenijburikov@MacBook-Pro-Evgenij.local>
2019-08-25 16:40:29 +0300

    Expansions

---
+++ app/Http/Controllers/ExpansionModuleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ExpansionModule;
+use Illuminate\Http\Request;
+
+class ExpansionModuleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ExpansionModule  $expansionModule
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ExpansionModule $expansionModule)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ExpansionModule  $expansionModule
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ExpansionModule $expansionModule)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ExpansionModule  $expansionModule
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ExpansionModule $expansionModule)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ExpansionModule  $expansionModule
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ExpansionModule $expansionModule)
+    {
+        //
+    }
+}
--- app/Sensor.php
+++ app/Sensor.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ExpansionModule extends Model
+class Sensor extends Model
 {
     //
 }
