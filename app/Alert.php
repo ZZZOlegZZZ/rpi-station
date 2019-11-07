3052ef2d6a809aa843df2b15ee30c6cc49aa0ad1
Евгений Буриков <evgenijburikov@MacBook-Pro-Evgenij-2.local>
2019-11-07 19:04:06 +0300

    PreRelease 1_10

--- app/Alert.php
+++ app/Alert.php
@@ -25,59 +25,20 @@ class Alert extends Model
     }
 
     public static function powerStatus(){
-      $module = ExpansionModule::where('alias','mercury-206')->first();
-      $measurement = new Measurement;
-      $rawData = $measurement->pollModule($module);
-      $sensors = [
-        "input_voltage" => [
-          "mask" => "POWER\\s+(-*\\d+\\.*\\d*)",
-          "rule" => '$value = $s_value;'
-        ],
-        "input_current" => [
-          "mask" => "POWER\\s+\\S+\\s+(-*\\d+\\.*\\d*)",
-          "rule" => '$value = $s_value;'
-        ]
-      ];
-      $data=[];
-
-      foreach($module->devices as $device){
-        foreach ($sensors as $param => $sensor){
-          preg_match(
-            "/".$sensor['mask']."/",
-            $rawData,
-            $matches
-          );
-
-
-          if (count($matches) > 1){
-            $s_value = $matches[1];
-
-
-            eval($sensor['rule']);
-
-            $data[$param] = $value;
-          }
-        }
-        if ($data['input_voltage'] && $data['input_current']){
-          return ['power_status'=> null];
-        } elseif($data['input_voltage'] && $data['input_current']==0){
-          return ['power_status'=>'Выключен автомат питания'];
-        } else {
-          return ['power_status'=>'Нет сети'];
-        }
+      $data = Power::firstOrFail(1);
+      if ($data->status == 0){
+        return ['power_status'=>null];
       }
-      // $data = StationData::orderBy('id', 'desc')->first()->data;
-      //
-      //
-      // if ($data->power_source<=1){
-      //   return ['power_status'=>null];
-      // }
-      //
-      // if (!$data->input_voltage){
-      //   return ['power_status'=>'Нет сети']; //Нет сети
-      // }
-      //
-      // return ['power_status'=>'Выключен автомат питания']; ////Выключен автомат питания
+
+      if ($data->status == 1){
+        return ['power_status'=>'Выключен автомат питания']; ////Выключен автомат питания
+      }
+
+      if ($data->status == 2){
+        return ['power_status'=>'Нет сети']; //Нет сети
+      }
+
+
     }
 
     public static function alertGroups(){
---
+++ app/Power.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Power extends Model
+{
+    protected $guarded = [];
+
+    public static function getStatus(){
+      $module = ExpansionModule::where('alias','mercury-206')->first();
+      $measurement = new Measurement;
+      $rawData = $measurement->pollModule($module);
+      $data=[];
+
+      foreach($module->devices as $device){
+        foreach (Expansion::sensors($module, $device) as $param => $sensor){
+          preg_match(
+            "/".Expansion::mask($device, $param)."/",
+            $rawData,
+            $matches
+          );
+
+
+          if (count($matches) > 1){
+            $s_value = $matches[1];
+
+
+            eval(Expansion::rule($device, $param));
+
+            $data[$param] = $value;
+          }
+        }
+        if ($data['input_voltage'] && $data['input_current']){
+          $status = 0;
+        } elseif($data['input_voltage'] && $data['input_current']==0){
+          $status = 1; //Автомат
+        } else {
+          $status = 2; //Сеть
+        }
+
+        Power::firstOrUpdate(['id' => 1], ['status' => $status]);
+      }
+    }
+}
