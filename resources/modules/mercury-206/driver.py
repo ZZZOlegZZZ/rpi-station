9313851ba86b4ea74ee3419cb8a7cb840e16aa75
Евгений Буриков <evgenijburikov@MacBook-Pro-Evgenij-2.local>
2019-11-25 11:02:05 +0300

    merc test2

--- resources/modules/mercury-206/driver.py
+++ resources/modules/mercury-206/driver.py
@@ -9,24 +9,30 @@ port = '/dev/ttyUSB0'
 if len(sys.argv)>1:
     port = sys.argv[1]
 
-conn = communications.open_serial(port=port, baudrate=9600, parity=serial.PARITY_NONE,
-    bytesize=8, stopbits=1, timeout=0.5)
-
-# try:
 
 i = 0
 t = True
 while t:
     i += 1
-    data = commands.instant_vcp(conn, 1)
 
-    if data:
-        break
+    try:
+        conn = communications.open_serial(port=port, baudrate=9600, parity=serial.PARITY_NONE,
+        bytesize=8, stopbits=1, timeout=0.5)
 
-    if i == 5:
-        exit()
+        data = commands.instant_vcp(conn, 1)
 
-    time.sleep(1)
+        if data:
+            break
+
+        if i == 5:
+            exit()
+
+        time.sleep(1)
+        i+=1
+    except:
+        i += 1
+
+
 
 data_string = 'POWER'
 
