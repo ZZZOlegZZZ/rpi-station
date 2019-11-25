b670269e3ccc062818c717e15f02d192d8161a4f
Евгений Буриков <evgenijburikov@MacBook-Pro-Evgenij-2.local>
2019-11-25 10:46:25 +0300

    pm test1

--- resources/modules/pow-mon/driver.py
+++ resources/modules/pow-mon/driver.py
@@ -21,22 +21,28 @@ try:
     tn485.close()
 
     tn232.write("link 1\n".encode('ascii'))
+    time.sleep(1)
     data = tn232.read_until(b"LINK OPENED").decode('ascii')
     print("LINK OPENED")
 
+
     tn232.write("\n".encode('ascii'))
+    time.sleep(1)
     data = tn232.read_until(b"ENTER OPEN>").decode('ascii')
     print("ENTER OPEN")
 
     tn232.write("open\n".encode('ascii'))
+    time.sleep(1)
     tn232.read_until(b"PM>").decode('ascii')
     print("PM")
 
     tn232.write("ALL\n".encode('ascii'))
+    time.sleep(1)
     data = tn232.read_until(b"PM>").decode('ascii')
     print("ALL")
 
     tn232.write(3*chr(27).encode('ascii'))
+    time.sleep(1)
     tn232.read_until(b"LINK CLOSED").decode('ascii')
     print("closed")
 
