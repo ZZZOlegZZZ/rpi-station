309a9bded3f9041883e5a2228aa5fe1dc2f068f7
Евгений Буриков <evgenijburikov@MacBook-Pro-Evgenij-2.local>
2019-11-25 10:55:30 +0300

    merc test1

--- resources/modules/mercury-206/driver.py
+++ resources/modules/mercury-206/driver.py
@@ -12,28 +12,28 @@ if len(sys.argv)>1:
 conn = communications.open_serial(port=port, baudrate=9600, parity=serial.PARITY_NONE,
     bytesize=8, stopbits=1, timeout=0.5)
 
-try:
+# try:
 
-    i = 0
-    t = True
-    while t:
-        i += 1
-        data = commands.instant_vcp(conn, 1)
+i = 0
+t = True
+while t:
+    i += 1
+    data = commands.instant_vcp(conn, 1)
 
-        if data:
-            break
+    if data:
+        break
 
-        if i == 5:
-            exit()
+    if i == 5:
+        exit()
 
-        time.sleep(1)
+    time.sleep(1)
 
-    data_string = 'POWER'
+data_string = 'POWER'
 
-    for value in data:
-        data_string += ' ' + str(value)
+for value in data:
+    data_string += ' ' + str(value)
 
-    print data_string;
+print data_string;
 
-except :
-   exit()
+# except :
+#    exit()
