132f8f68f8262a8a1ebf1a0ed23618c861bb7e8e
Евгений Буриков <evgenijburikov@MacBook-Pro-Evgenij-2.local>
2020-02-10 16:02:44 +0300

    Panel Size Tgl

--- resources/modules/wxt-520/driver.py
+++ resources/modules/wxt-520/driver.py
@@ -3,7 +3,9 @@ import time
 import telnetlib
 
 host = '192.168.1.1'
+# host = '37.29.36.37'
 port = 4002
+
 
 if len(sys.argv)>1:
     host = sys.argv[1]
@@ -12,14 +14,13 @@ if len(sys.argv)>1:
 try:
     tn = telnetlib.Telnet(host, port)
 
-
-    tn.write("0R\n".encode('ascii'))
-    tn.write("0R\n".encode('ascii'))
-
+    tn.write((chr(27)+chr(27)+chr(27)+"\r\n").encode('ascii'))
     time.sleep(1)
+    tn.write("0R\r\n".encode('ascii'))
+    time.sleep(2)
 
-    data = tn.read_some().decode('ascii')
 
+    data = tn.read_very_eager().decode('ascii')
     print(data)
 
 
