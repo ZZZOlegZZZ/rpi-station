7d5d363e31821e292845dd08cc1e10337f5a8ce1
Евгений Буриков <evgenijburikov@MacBook-Pro-Evgenij-2.local>
2020-04-02 08:30:09 +0300

    wh24p module

--- resources/modules/wh24p/driver.py
+++ resources/modules/wh24p/driver.py
@@ -6,18 +6,15 @@ cursor = dbconn.cursor()
 
 power = cursor.execute("SELECT json_extract(settings, '$.power') from configurations").fetchall()[0][0]
 
-print (type(int(power)))
-
-
-# if power>0:
-#     last_data = cursor.execute(
-#         """SELECT
-#             measured_at,
-#             json_extract(data, '$.t_air'),
-#             json_extract(data, '$.humidity'),
-#             json_extract(data, '$.pressure'),
-#             json_extract(data, '$.uv'),
-#             json_extract(data, '$.light')
-#         from plugin_wh24p"""
-#     ).fetchall[0]
-#     print(last_data)
+if int(power)>0:
+    last_data = cursor.execute(
+        """SELECT
+            measured_at,
+            json_extract(data, '$.t_air'),
+            json_extract(data, '$.humidity'),
+            json_extract(data, '$.pressure'),
+            json_extract(data, '$.uv'),
+            json_extract(data, '$.light')
+        from plugin_wh24p"""
+    ).fetchall[0]
+    print(last_data)
