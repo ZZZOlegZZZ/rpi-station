57be7c572d5ff75dc237be8174922aef148dc63c
Евгений Буриков <evgenijburikov@MacBook-Pro-Evgenij-2.local>
2020-04-02 09:15:37 +0300

    wh24p module

--- resources/modules/wh24p/driver.py
+++ resources/modules/wh24p/driver.py
@@ -4,30 +4,36 @@ import json
 dbconn = sqlite3.connect('/var/www/vhosts/rpi-station/database/rpi-station.sqlite')
 cursor = dbconn.cursor()
 
-power = cursor.execute("SELECT json_extract(settings, '$.power') from configurations").fetchall()[0][0]
+cursor.execute("SELECT json_extract(settings, '$.power'), json_extract(settings, '$.poll_intv') from configurations").fetchall()[0][0]
+power = cursor.fetchall()[0][0]
+poll_intv = cursor.fetchall()[0][1]
 
-if int(power)>0:
-    last_data = cursor.execute(
-        """SELECT
-            measured_at,
-            json_extract(data, '$.t_air'),
-            json_extract(data, '$.humidity'),
-            json_extract(data, '$.pressure'),
-            json_extract(data, '$.uv'),
-            json_extract(data, '$.light')
-        from plugin_wh24p
-        order by measured_at desc limit 1"""
-    ).fetchall()[0]
+intv = power if int(power)>0 else poll_intv
 
-    accumulated_data = cursor.execute(
-        """SELECT
-            avg(json_extract(data, '$.wind_dir')),
-            avg(json_extract(data, '$.wind_speed')),
-            max(json_extract(data, '$.wind_gusts')),
-            sum(json_extract(data, '$.rainfall'))
-        from plugin_wh24p
-        where measured_at > DATETIME(datetime("now","-%s minute"))""" % (power)
-    ).fetchall()[0]
+last_data = cursor.execute(
+    """SELECT
+        measured_at,
+        json_extract(data, '$.t_air'),
+        json_extract(data, '$.humidity'),
+        json_extract(data, '$.pressure'),
+        json_extract(data, '$.uv'),
+        json_extract(data, '$.light')
+    from plugin_wh24p
+    order by measured_at desc limit 1"""
+).fetchall()[0]
+
+accumulated_data = cursor.execute(
+    """SELECT
+        avg(json_extract(data, '$.wind_dir')),
+        avg(json_extract(data, '$.wind_speed')),
+        max(json_extract(data, '$.wind_gusts')),
+        sum(json_extract(data, '$.rainfall'))
+    from plugin_wh24p
+    where measured_at > DATETIME(datetime("now","-%s minute"))""" % (intv)
+).fetchall()[0]
+
+cursor.execute('DELETE from plugin_wh24p;')
+dbconn.commit()
 
 print("t_air " + str(round(float(last_data[1]), 1)))
 print("humidity " + str(round(float(last_data[2]), 1)))
