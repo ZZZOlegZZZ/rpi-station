4a6cf4be2d973833eba592112f243c556874b6b4
Евгений Буриков <evgenijburikov@MacBook-Pro-Evgenij-2.local>
2020-04-02 08:28:32 +0300

    wh24p module

--- resources/modules/wh24p/driver.py
+++ resources/modules/wh24p/driver.py
@@ -4,17 +4,20 @@ import json
 dbconn = sqlite3.connect('/var/www/vhosts/rpi-station/database/rpi-station.sqlite')
 cursor = dbconn.cursor()
 
-power = int(cursor.execute("SELECT json_extract(settings, '$.power') from configurations").fetchall()[0][0])
+power = cursor.execute("SELECT json_extract(settings, '$.power') from configurations").fetchall()[0][0]
 
-if power>0:
-    last_data = cursor.execute(
-        """SELECT
-            measured_at,
-            json_extract(data, '$.t_air'),
-            json_extract(data, '$.humidity'),
-            json_extract(data, '$.pressure'),
-            json_extract(data, '$.uv'),
-            json_extract(data, '$.light')
-        from plugin_wh24p"""
-    ).fetchall[0]
-    print(last_data)
+print (type(power))
+
+
+# if power>0:
+#     last_data = cursor.execute(
+#         """SELECT
+#             measured_at,
+#             json_extract(data, '$.t_air'),
+#             json_extract(data, '$.humidity'),
+#             json_extract(data, '$.pressure'),
+#             json_extract(data, '$.uv'),
+#             json_extract(data, '$.light')
+#         from plugin_wh24p"""
+#     ).fetchall[0]
+#     print(last_data)
