f632ef7e46f487dee77617c4f2f7ffc106a8f784
Евгений Буриков <evgenijburikov@MacBook-Pro-Evgenij-2.local>
2020-04-02 08:25:58 +0300

    wh24p module

--- resources/modules/wh24p/driver.py
+++ resources/modules/wh24p/driver.py
@@ -4,7 +4,7 @@ import json
 dbconn = sqlite3.connect('/var/www/vhosts/rpi-station/database/rpi-station.sqlite')
 cursor = dbconn.cursor()
 
-power = cursor.execute("SELECT json_extract(settings, '$.power') from configurations").fetchall()[0][0]
+power = int(cursor.execute("SELECT json_extract(settings, '$.power') from configurations").fetchall()[0][0])
 
 if power>0:
     last_data = cursor.execute(
