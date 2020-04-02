7ad74a542ee20aecfe0ae5e931cb4211ff116c0e
Евгений Буриков <evgenijburikov@MacBook-Pro-Evgenij-2.local>
2020-04-02 09:50:31 +0300

    wh24p module

--- app/PushClient.php
+++ app/PushClient.php
@@ -9,35 +9,33 @@ class PushClient extends Model
 {
     protected $guarded=[];
 
-    public static function push() {
+    public static function pushData() {
+      $gzclient = new GzClient();
+
       foreach (self::all() as $client){
         $station_id = $client->station_id
           ?$client->station_id
           :Configuration::find(1)->station_id;
-      }
 
-      $gzclient = new GzClient();
+        foreach(StationData::unsent($client) as $data) {
+          $response = $gzclient->request($client->method,
+            'http://'.
+            $client->host.":".$client->port."/".
+            $client->uri, [
+              'form_params' => [
+                'station_id' => $station_id,
+                'measured_at' => date("Y-m-d H:i:s", strtotime($data->measured_at)),
+                'data' => json_encode($data->data),
+            ]
+          ]);
 
-      foreach(StationData::unsent($client) as $data) {
-        $response = $gzclient->request($client->method,
-          'http://'.
-          $client->host.":".$client->port."/".
-          $client->uri, [
-            'form_params' => [
-              'station_id' => $station_id,
-              'measured_at' => date("Y-m-d H:i:s", strtotime($data->measured_at)),
-              'data' => json_encode($data->data),
-          ]
-        ]);
-
-        if($response->getStatusCode() == 201) {
-          $client->last_sent_data_id = $data->id;
-          $client->save();
-        } else {
-          break; 
+          if($response->getStatusCode() == 201) {
+            $client->last_sent_data_id = $data->id;
+            $client->save();
+          } else {
+            break;
+          }
         }
-
-
       }
     }
 }
