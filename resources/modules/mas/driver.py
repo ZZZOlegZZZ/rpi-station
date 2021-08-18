f5fea490fd145d24869e815e152362c1b89d4954
Evgenii Burikov <evgenii.burikov@gmail.com>
2021-08-18 14:34:30 +0300

    mas driver

---
+++ resources/modules/mas/driver.py
@@ -0,0 +1,30 @@
+import sys
+import time
+import telnetlib
+
+host = '192.168.1.26'
+port = 4002
+
+if len(sys.argv)>1:
+    host = sys.argv[1]
+    port = sys.argv[2]
+
+try:
+    tn = telnetlib.Telnet(host, port)
+
+
+    # tn.write(chr(27).encode('ascii'))
+    # time.sleep(1)
+    tn.write("@2 MES 2\n".encode('ascii'))
+
+    data = tn.read(100).decode('ascii')
+
+    tn.write("@2 MES 3\n".encode('ascii'))
+
+    data = data + tn.read(100).decode('ascii')
+
+    print(data)
+
+
+except:
+    exit()
