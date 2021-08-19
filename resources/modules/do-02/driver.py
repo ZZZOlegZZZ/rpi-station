b9dc25fbb2b5f772f22079f4d41a6a29b5bd9ade
Evgenii Burikov <evgenii.burikov@gmail.com>
2021-08-19 08:47:06 +0300

    do driver fix

--- resources/modules/do-02/driver.py
+++ resources/modules/do-02/driver.py
@@ -4,15 +4,20 @@ import telnetlib
 
 host = '192.168.1.1'
 port = 4001
+id = None
 
 if len(sys.argv)>1:
     host = sys.argv[1]
     port = sys.argv[2]
+    id = sys.argv[3]
 
 try:
     tn = telnetlib.Telnet(host, port)
 
-    tn.write("M0\r\n".encode('ascii'))
+    if (id):
+        tn.write("@" + id + " M0\r\n".encode('ascii'))
+    else:
+        tn.write("M0\r\n".encode('ascii'))
     time.sleep(3)
 
     data = tn.read_very_eager().decode('ascii')
