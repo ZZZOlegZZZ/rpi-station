ee2edabacb6f426f15ed3d4f99c1e328e8bb8ecd
Evgenii Burikov <evgenii.burikov@gmail.com>
2022-08-31 11:49:37 +0300

    dtv-05 driver added

---
+++ resources/modules/dtv-05/driver.py
@@ -0,0 +1,24 @@
+import sys
+import time
+import telnetlib
+
+host = '192.168.1.1'
+port = 4002
+
+if len(sys.argv)>1:
+    host = sys.argv[1]
+    port = sys.argv[2]
+
+try:
+    tn = telnetlib.Telnet(host, port)
+
+    tn.write("@5 M\r\n".encode('ascii'))
+    time.sleep(3)
+
+    data = tn.read_very_eager().decode('ascii')
+
+    print(data)
+
+
+except:
+    exit()
