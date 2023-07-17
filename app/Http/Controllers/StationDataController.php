9d84b6c30594c91853f504c0f625f7591a5aedb3
Evgenii Burikov <burikov@xologie.ru>
2023-07-17 17:49:56 +0300

    hist fix

--- app/Http/Controllers/StationDataController.php
+++ app/Http/Controllers/StationDataController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Measurement;
 use App\Expansion;
 use App\StationData;
@@ -9,38 +10,40 @@ use Illuminate\Http\Request;
 
 class StationDataController extends Controller
 {
-    public function index(Request $request){
-      if ($request->ignore_is_read){
-        $station_data = StationData::all();
-      } else {
-        $station_data = StationData::where('is_read', 0)->get();
-      }
-
-      if ($request->mark_as_read){
-        foreach($station_data as $data_chunk){
-          $data_chunk->update(['is_read' => true]);
+    public function index(Request $request)
+    {
+        if ($request->ignore_is_read) {
+            $station_data = StationData::all();
+        } else {
+            $station_data = StationData::where('is_read', 0)->get();
         }
-      }
 
-      return $station_data;
+        if ($request->mark_as_read) {
+            foreach ($station_data as $data_chunk) {
+                $data_chunk->update(['is_read' => true]);
+            }
+        }
+
+        return $station_data;
     }
 
-    public function lastData($ndays){
-      return StationData::whereRaw(
-          'measured_at > datetime("now","-'.$ndays.' day")'
+    public function lastData($ndays)
+    {
+        return StationData::whereRaw(
+            'measured_at > datetime("now","-' . $ndays . ' day")'
         )->get();
     }
 
-    public function history(Request $request){
-      $request->validate([
-        'startDate' => 'required|date',
-        'endDate' => 'required|date',
-      ]);
+    public function history(Request $request)
+    {
+        $request->validate([
+            'startDate' => 'required|date',
+            'endDate' => 'required|date',
+        ]);
 
-      return StationData::whereRaw(
-          'measured_at>=\''.$request->startDate.
-          '\' AND measured_at<=\''.$request->endDate.'\''
-        )->get();
+        return StationData::whereRaw(
+            'measured_at>=\'' . $request->startDate .
+                '\' AND measured_at<=\'' . $request->endDate . '\''
+        )->orderBy('measured_at', 'desc')->get();
     }
-
 }
